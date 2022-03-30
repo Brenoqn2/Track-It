3a85eb0e8a6d9186cfa6ae444e2894fa0bfe215f
Brenoqn2 <brenoqn2@gmail.com>
2022-03-29 22:42:18 -0300

    Layout Register/Login pages

---
+++ src/components/RegisterPage.jsx
@@ -0,0 +1,87 @@
+import { Link } from "react-router-dom";
+import logo from "../assets/logo-trackit.png";
+import styled from "styled-components";
+
+export default function RegisterPage() {
+  return (
+    <Main>
+      <Img src={logo} alt="logo-trackit"></Img>
+      <Form>
+        <Input type="email" placeholder="email"></Input>
+        <Input type="password" placeholder="senha"></Input>
+        <Input type="text" placeholder="nome"></Input>
+        <Input type="url" placeholder="foto"></Input>
+        <Button type="submit">Cadastrar</Button>
+      </Form>
+      <Link to="/">
+        <P>Já tem uma conta? Faça login!</P>
+      </Link>
+    </Main>
+  );
+}
+
+const Main = styled.main`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const Img = styled.img`
+  width: 180px;
+  margin-top: 68px;
+`;
+
+const Form = styled.form`
+  width: 303px;
+  display: flex;
+  flex-direction: column;
+`;
+
+const Input = styled.input`
+  background: #ffffff;
+  border: 1px solid #d5d5d5;
+  box-sizing: border-box;
+  border-radius: 5px;
+  height: 45px;
+  width: 100%;
+  margin-bottom: 6px;
+  padding-left: 11px;
+
+  ::placeholder {
+    font-family: "Lexend Deca";
+    font-style: normal;
+    font-weight: 400;
+    font-size: 19.976px;
+    line-height: 25px;
+
+    color: #dbdbdb;
+  }
+`;
+
+const Button = styled.button`
+  width: 100%;
+  height: 45px;
+  background: #52b6ff;
+  border-radius: 4.63636px;
+  font-family: "Lexend Deca";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 20.976px;
+  line-height: 26px;
+  text-align: center;
+  color: #ffffff;
+  border: none;
+  margin-bottom: 25px;
+`;
+
+const P = styled.p`
+  font-family: "Lexend Deca";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 13.976px;
+  line-height: 17px;
+  text-align: center;
+  text-decoration-line: underline;
+
+  color: #52b6ff;
+`;
